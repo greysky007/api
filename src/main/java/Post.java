280c3f35eca6496394b8f1e0f30ff26148ea5908
greysky <antonysnork@gmail.com>
2022-07-10 12:28:44 +0300

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    public String name;
+    public String passNum;
+    public String patronymic;
+    public String phoneNum;
+    public String surname;
+    public boolean subscription;
+
+    public FormDate birthday;
+
+
+}
